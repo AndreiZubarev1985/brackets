fdb53f62c1d8f327cb526bde471b347862ecd511
Andrei <andreizubarev1985@gmail.com>
2020-03-21 20:56:46 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+  let typestr = String(str);
+  while (typestr.length > 0) {
+      let isConfig = false;
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          let config = bracketsConfig[j];
+          let n = String(config[0]) + String(config[1]);
+          let p = typestr.indexOf(n);
+          if (p !== -1) {
+              typestr = typestr.replace(n, '');
+              isConfig = true;
+          }
+
+      }
+      if (!isConfig) return isConfig;
+  }
+  return typestr.length === 0;
 }
